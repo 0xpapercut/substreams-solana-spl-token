4c879aa7505b5b7aa177f8a5ba5821baabaf0c82
0xpapercut <0xpapercut@gmail.com>
2024-05-01 23:42:40 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod instruction;
+pub use instruction::*;
 
 pub const SOL_MINT: &str = "So11111111111111111111111111111111111111112";
 pub const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
